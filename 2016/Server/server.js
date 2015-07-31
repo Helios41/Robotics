69efd709f0ffb74efcf9bccc9c10f7bcbd98f3f2
Helios41 <jonny41@msn.com>
2015-07-31 19:09:40 -0400

    mainly visuals

--- 2016/Server/server.js
+++ 2016/Server/server.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var fs = require('fs');
 var https = require('http'); //require('http');
-//TODO: https & socket.io
+var socketio = require('socket.io');
 
 var app = express();
+
+function LoadTeamInfo(team)
+{
+   var file_path = "./teams/t" + team + ".json";
+   var is_valid = fs.existsSync(file_path);
+   var team_json = "NOJSON";
+   
+   if(is_valid)
+   {
+      team_json = fs.readFileSync(file_path).toString();
+      console.log(team_json);
+   }
+   else
+   {
+      console.log("no data for team " + team);
+   }
+   
+   var result = 
+   {
+      valid: is_valid,
+      json: is_valid ? JSON.parse(team_json) : {}
+   };
+   
+   return result;
+}
+
+function SetTeamInfo(team, info)
+{
+   var file_path = "./teams/t" + team + ".json";
+   fs.writeFileSync(file_path, JSON.stringify(info));
+}
+
+function ProcessCommand(command)
+{
+   try
+   {
+      var json = JSON.parse(command);
+      SetTeamInfo(json.team, json);
+   }
+   catch(e) {}
+}
 
 app.get('/', function(req, res)
 {
@@ -20,6 +61,29 @@ app.get('/scout/', function(req, res)
    res.send(page_html);
 });
 
+app.get('/scout/display:team', function(req, res)
+{
+   var page_html = fs.readFileSync("./pages/scouting_display.html").toString();
+   var team = parseInt(req.params.team).toString();
+   
+   var team_info = LoadTeamInfo(team);
+   
+   if(team_info.valid)
+   {
+      for(var key in team_info.json)
+      {
+         page_html = page_html + "<p>" + key + ": " + team_info.json[key] + "</p>";
+      }
+   }
+   else
+   {
+      page_html = page_html + "<p>Searching for Team " + team + "</p><p>No data found!</p>";
+   }
+   
+   page_html = page_html.replace("[TEAM]", team);
+   res.send(page_html);
+});
+
 /*
 var HttpsOptions = 
 {
@@ -28,8 +92,24 @@ var HttpsOptions =
 };
 */
 
-//var server = https.createServer(HttpsOptions, app);
-var server = https.createServer(app);
+var server = https.createServer(/*HttpsOptions,*/ app);
+var io = socketio(server);
+
+io.on('connection', function(socket)
+{
+   io.emit('recive_value', "--connected");
+   
+   socket.on('submit_value', function(msg)
+   {
+      ProcessCommand(msg);
+      io.emit('recive_value', msg);
+   });
+   
+   socket.on('disconnect', function()
+   {
+      io.emit('recive_value', "--disconnected");
+   });
+});
 
 server.listen(3000, function()
 {
